87d7d4b3e4431e56853334732d1df4ea3556fbe4
Diego Celis <diegofercelis@gmail.com>
2021-03-20 01:03:33 -0300

    Validacoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,21 @@ export class AppComponent {
 
   //new:
   constructor (private usuaService: UsuarioService) {
-    usuaService.getUsuario().subscribe(usuario_preenchido => this.usuario_preenchido = usuario_preenchido)
+    // usuaService.getUsuario().subscribe(usuario_preenchido => this.usuario_preenchido = usuario_preenchido) 
+    //COMENTADO PARA DEIXAR O FORMULARIO VAZIO PARA A GENTE PREENCHER
   }
 
 
   funcao_onSubmit (formulariezinho){
+    if (formulariezinho.form.status == 'INVALID')
+    {
+      alert ('O formulario deve ser preenchido corretamente! Dados incorretos!')
+    }
+    else
+    {
+      alert ('dados enviados com sucesso!')
     console.log(formulariezinho.form.value);
+    }
+    
   }
 }
